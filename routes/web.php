11340d1ebfd49510f3803cb354a96c09fd22d14e
Bent Brüggemann <mail@bent-brueggemann.de>
2019-08-15 21:34:32 +0200

    mass create models + controllers + migrations

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,21 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//users
+Route::get('/logout', 'UserController@logout')->name('logout');
+Route::get('/login', 'UserController@login')->name('login');
+Route::get('/register', 'UserController@register')->name('register');
+Route::get('/{username}', 'UserController@profile')->name('profile');
+
+//stories
+Route::get('/{username}/stories', 'StoryController@stories')->name('stories');
+Route::get('/{username}/{story}', 'StoryController@profile')->name('story');
+Route::get('/{username}/{story}/{id}', 'StoryController@profile')->name('storyDetail');
+Route::put('/story', 'StoryController@putStory');
+Route::patch('/story', 'StoryController@patchStory');
+Route::delete('/story', 'StoryController@deleteStory');
+
+//subscriptions
+Route::put('/subscription/{storyId}', 'SubscriptionController@putSubscription');
+Route::get('/subscriptions', 'SubscriptionController@getSubscriptions');
